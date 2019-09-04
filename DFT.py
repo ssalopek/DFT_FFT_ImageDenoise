68fd064044ee38ea12efc5f3957ddd55cc15398e
Stjepan <ssalopek@etfos.hr>
2019-09-04 09:09:31 +0200

    Final commit

--- DFT.py
+++ DFT.py
@@ -1,50 +1,55 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import time
 
-img = plt.imread('C:/FaksGit/FourierFilter/lenna.png').astype(float)
-img_float32 = np.float32(img)
+start = time.time()
+#Load source image
+image_source = plt.imread('C:/FaksGit/FourierFilter/TestImages/moonlanding.png').astype(float)
+img_float32 = np.float32(image_source)
 
+#Plot input image
 plt.figure()
-plt.title('Original image')
-plt.imshow(img, plt.cm.gray)
+plt.imshow(image_source, plt.cm.gray)
 plt.xticks([]), plt.yticks([])
+plt.title('Original image')
 
-dft = cv2.dft(img_float32, flags = cv2.DFT_COMPLEX_OUTPUT)
-dft_shift = np.fft.fftshift(dft)
+#Performs a forward or inverse Discrete Fourier transform of a 1D or 2D floating-point array
+dft = cv2.dft(img_float32, flags = cv2.DFT_COMPLEX_OUTPUT) 
+#Shift the zero-frequency component to the center of the spectrum.
+dft_shift = np.fft.fftshift(dft) 
 
-rows, cols = img.shape
-crow, ccol = rows/2 , cols/2  # center
+rows, cols = image_source.shape
+crow, ccol = rows/2 , cols/2  # centar
+
+keep_fraction = 60
 
 # create a mask first, center square is 1, remaining all zeros
 mask = np.zeros((rows, cols, 2), np.uint8)
-mask[int(crow)-30:int(crow)+30, int(ccol)-30:int(ccol)+30] = 1
+mask[int(crow)-keep_fraction:int(crow)+keep_fraction, int(ccol)-keep_fraction:int(ccol)+keep_fraction] = 1
 
 # apply mask and inverse DFT
-fshift = dft_shift*mask
+fshift = dft_shift * mask
+
+#The inverse of fftshift. Although identical for even-length x, 
+#the functions differ by one sample for odd-length x
 f_ishift = np.fft.ifftshift(fshift)
+
+#Calculates the inverse Discrete Fourier Transform of a 1D or 2D array.
 img_back = cv2.idft(f_ishift)
+
+#Calculates the magnitude of 2D vectors
 img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
 
-"""
-cv2.imshow('Reconstructed image DFT', img_back)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('moonlanding_DFT.png',img_back)
-"""
-"""
-plt.imshow(img_back, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
-plt.savefig('moonlandingDFT.png')
-"""
+end = time.time()
+print("Time:", end-start)
 
-fig = plt.figure()
+fig = plt.figure(figsize=(20,10))
 plt.imshow(img_back, plt.cm.gray)
 plt.xticks([]), plt.yticks([])
 plt.title('Reconstructed Image DFT')
 plt.show()
-fig.savefig('lennaDFT.png')
+fig.savefig('baboon_gauss60_DFT60.png')
 
 
 
--- FFT.py
+++ FFT.py
@@ -1,52 +1,59 @@
-"""http://scipy-lectures.org/intro/scipy/auto_examples/solutions/plot_fft_image_denoise.html"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import fftpack
 from matplotlib.colors import LogNorm 
+import cv2
+import time
 
-image_source = plt.imread('C:/FaksGit/FourierFilter/man.png').astype(float)
+start = time.time()
+#Load input image
+image_source = cv2.imread('C:/FaksGit/FourierFilter/TestImages/man.png')
+gray_image = cv2.cvtColor(image_source, cv2.COLOR_BGR2GRAY)
 
+#Plot input image
 plt.figure()
-plt.imshow(image_source, plt.cm.gray)
+plt.imshow(gray_image, plt.cm.gray)
 plt.xticks([]), plt.yticks([])
 plt.title("Original image")
 
-image_fft = fftpack.fft2(image_source)
+#Return the two-dimensional discrete Fourier transform of the 2-D argument x.
+image_fft = fftpack.fft2(gray_image) 
 
+#Logaritmic map
 def show_spectrum(image_fft):
     plt.imshow(np.abs(image_fft), norm=LogNorm(vmin=5))
-    plt.colorbar() 
+    plt.colorbar()  #Add colorbar 
 
+#Plot FT input image
 plt.figure()
 show_spectrum(image_fft)
 plt.title("Fourier transform")
 
-keep_fraction = 0.1
+keep_fraction = 0.3  #keep fraction (u oba smijera)
 image_fft2 = image_fft.copy()
-row, col = image_fft2.shape
+row, col = image_fft2.shape  #get the current shape of an array
 
-image_fft2[int(row*keep_fraction):int(row*(1-keep_fraction))] = 0
+#Set on zero all rows with index between row*keep_fraction and row*(1-keep_fraction)
+image_fft2[int(row*keep_fraction):int(row*(1-keep_fraction))] = 0 
+#Similar for columns
 image_fft2[:, int(col*keep_fraction):int(col*(1-keep_fraction))] = 0
 
+#Plot spectrum
 plt.figure()
 show_spectrum(image_fft2)
 plt.title('Filtered Spectrum')
 
+#Return inverse two-dimensional discrete Fourier transform of arbitrary type sequence x
 image_new = fftpack.ifft2(image_fft2).real
 
-"""
-cv2.imshow('Reconstructed image FFT', image_new)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite('moonlanding_FFT.png',image_new)
-"""
+end = time.time()
+print("Time:" ,end - start)
 
-fig = plt.figure()
+fig = plt.figure(figsize=(20,10))
 plt.imshow(image_new, plt.cm.gray)
 plt.xticks([]), plt.yticks([])
 plt.title('Reconstructed Image FFT')
-fig.savefig('manFFT.png')
+fig.savefig('baboon_gauss60.3FFT.png')
 
 
 
